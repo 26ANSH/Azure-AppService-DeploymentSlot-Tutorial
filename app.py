e825fb4e82ae214a19394e8f650533fe8600f0f3
26ANSH <anshvidyabhanu8@gmail.com>
2022-05-28 19:43:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def hello():
     user = os.environ['USER_NAME'] if 'USER_NAME' in os.environ else 'World'
     return render_template('hello.html', user=user)
 
-app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
